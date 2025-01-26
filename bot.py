f0e518c7d009689ae5cbe13e2d1241df164c26aa
mrchain256 <rnaldi1812@gmail.com>
2025-01-26 12:06:13 +0700

    Remove unused function

--- bot.py
+++ bot.py
@@ -40,24 +40,6 @@ class LayerEdgeRegistration:
             logger.error(f"Invalid proxy URL: {proxy_url}. Error: {e}")
             return None
 
-    """async def validate_invite(self, session, proxy_url):
-        invite_url = 'https://referral.layeredge.io/api/referral/verify-referral-code'
-        headers = {
-            'accept': 'application/json',
-            'accept-encoding': 'gzip, deflate, br, zstd',
-            'accept-language': 'en-US,en;q=0.9,id;q=0.8',
-            'content-type': 'application/json',
-            'origin': 'https://dashboard.layeredge.io',
-            'user-agent': random_user_agent
-        }
-        payload = {'code': 'dB3KLziw'}
-
-        async with session.post(invite_url, json=payload, headers=headers, proxy=proxy_url) as response:
-            if response.status == 200:
-                result = await response.json()
-                return result.get('success', False)
-            return False"""
-
     async def register_wallet(self, session, wallet_address, proxy_url):
         try:
             register_url = 'https://referral.layeredge.io/api/referral/register-wallet/dB3KLziw'
@@ -73,7 +55,6 @@ class LayerEdgeRegistration:
 
             async with session.post(register_url, json=payload, headers=headers, proxy=proxy_url) as response:
                 # Custom response decoding for Brotli
-                content_encoding = response.headers.get('content-encoding', '')
                 if 'br' in content_encoding:
                     response_body = await response.read()
                     decoded_body = brotli.decompress(response_body)
@@ -98,7 +79,6 @@ class LayerEdgeRegistration:
                 for proxy_url in self.proxies:
                     parsed_proxy = self.parse_proxy_url(proxy_url)
                     print(f"Connected with proxy: {parsed_proxy}")
-                    # await self.validate_invite(session, parsed_proxy)
                     register_wallet = await self.register_wallet(session, wallet_address, parsed_proxy)
                     if register_wallet == True:
                         break
